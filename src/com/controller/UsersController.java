95039b3bebf6509df4c95b6e4a478569ba54a8d2
JiLoveZn <2031265447@qq.com>
2018-10-21 18:09:37 +0800

    UsersController

--- src/com/controller/UsersController.java
+++ src/com/controller/UsersController.java
@@ -1,5 +1,11 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.service.UsersService;
+
 /**  
  * @author 陈吉
  * @version 创建时间：2018年10月20日 上午11:13:25
@@ -8,4 +14,7 @@ package com.controller;
 
 public class UsersController {
 
+	@Autowired
+	private UsersService usersService;
+	@RequestMapping(value="/register.action",method=RequestMethod.POST)
 }
